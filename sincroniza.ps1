9b94142b087c4f6f16c8f55b18d13d30337b4072
albeertocardenass <albertocardenaspalomo@gmail.com>
2025-09-29 10:13:11 +0200

    Hola

---
+++ sincroniza.ps1
@@ -0,0 +1,8 @@
+###############################################################
+#########	AUTOMATIZACIóN SCRIPT GIT 	###############
+###############################################################
+
+$m =read-host -prompt "Introduce mensaje"
+git add .
+git commit -m $m
+git push --force origin main
